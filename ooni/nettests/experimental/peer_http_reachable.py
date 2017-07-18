d9e730adc8d147acf1d6af43e55dfd27549f40da
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2017-07-18 16:19:22 +0200

    Update HTTP reachability probe to iterate over new format items
    Old entries are discarded if the protocol does not match or they are too old,
    which depends on whether they use NAT or not.

--- ooni/nettests/experimental/peer_http_reachable.py
+++ ooni/nettests/experimental/peer_http_reachable.py
@@ -2,7 +2,19 @@ from twisted.python import usage
 from ooni.templates import httpt
 from ooni.utils import log
 
+import collections
+import time
+
 from datetime import datetime, timedelta
+
+
+# Discard peer entries older than this many seconds.
+MAX_PEER_AGE_SECS_NONAT = (7 - 1) * 24 * 60 * 60  # public IP: 6 days, one less than max server age
+MAX_PEER_AGE_SECS_NAT = 2 * 24 * 60 * 60  # behind NAT: 2 days, a guess on frequency of public IP changes
+
+
+# A peer entry with a time stamp, transport address, protocol and a tuple of flags.
+PeerEntry = collections.namedtuple('PeerEntry', 'ts addr proto flags')
 
 class PeerHttpReachable(httpt.HTTPTest):
     """
@@ -19,19 +31,35 @@ class PeerHttpReachable(httpt.HTTPTest):
     requiresRoot = False
     requiresTor = False
 
+    def _parsePeerEntry(self, data):
+        """Parse `data` and return a `PeerEntry`."""
+        splitted = data.split()
+        return PeerEntry(ts=float(splitted[0]),
+                         addr=splitted[1],
+                         proto=splitted[2], flags=tuple(splitted([3:])))
+
+    def inputProcessor(self, filename):
+        """Iterate over each `PeerEntry` in the peers file."""
+        now = time.time()
+        for l in super(PeerHttpReachable, self).inputProcessor(filename):
+            peer = self._parsePeerEntry(l)
+            # Only consider entries not older than max peer age
+            # (which depends on whether the peer is behind NAT).
+            max_peer_age = MAX_PEER_AGE_SECS_NONAT if b'nonat' in peer.flags else MAX_PEER_AGE_SECS_NAT
+            if (now - peer.ts) < max_peer_age and peer.proto == 'HTTP':
+                yield peer
+
     def setUp(self):
         """
         Check for inputs.
         """
         self.localOptions['withoutbody'] = 1
-        log.msg(str(self.input.split()))
-        url = self.input
-        if '/' not in url:  # fix ``PUB_ADDR:PORT`` entries
-            url = url + '/'
-        if not url.beginswith('http://'):  # fix ``PUB_ADDR:PORT[/?QUERY_ARGS]`` entries
-            url = 'http://' + url
-        self.http_url = url
+        peer = self.input
+        log.msg(str(peer))
+        self.http_url = 'http://%s/' % peer.addr
         self.report['http_success'] = False
+        self.report['peer_ts'] = peer.ts
+        self.report['peer_nat'] = b'nonat' not in peer.flags
 
     def test_http_speed(self):
         """
